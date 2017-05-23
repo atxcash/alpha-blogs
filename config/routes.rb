bc0aba6864c166739abb9958fb022509945065a3
Rails Udemy Course <srcatx@gmail.com>
2017-05-23 04:34:01 +0000

    Create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   #get 'pages/home', to: 'pages#home'
   root 'pages#home'
   get 'about', to: 'pages#about'
-
+  
+  resources :articles
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
